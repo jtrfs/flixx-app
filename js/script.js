e17fc11b82a2383c7b29028dd6e09acfcec29073
Jan Varmuza <jan.varmuza@gmail.com>
2024-02-04 13:13:17 +0100

    movie details page and details page backdrop

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const global = {
   spinner: document.querySelector('.spinner'),
   popularMovies: document.getElementById('popular-movies'),
   popularShows: document.getElementById('popular-shows'),
+  movieDetails: document.getElementById('movie-details'),
 };
 
 // fetch the popular movies
@@ -60,6 +61,96 @@ const displayPopularShows = async () => {
   });
 };
 
+//fetch movie details: https://api.themoviedb.org/3/movie/{movie_id}
+const displayMovieDetails = async () => {
+  const pageSearchParams = new URL(window.location).searchParams;
+  const id = pageSearchParams.get('id');
+  const movie = await fetchApiData(`movie/${id}`);
+  const {
+    budget,
+    genres,
+    homepage,
+    overview,
+    poster_path: poster,
+    production_companies: companies,
+    release_date: release,
+    revenue,
+    runtime,
+    status,
+    title,
+    vote_average: rating,
+    backdrop_path: backdrop,
+  } = movie;
+
+  // Overlay for background image
+  displayBackgroundImage('movie', backdrop);
+
+  const divTop = document.createElement('div');
+  const divBottom = document.createElement('div');
+  divTop.classList.add('details-top');
+  divBottom.classList.add('details-bottom');
+  divTop.innerHTML = `
+    <div>
+      ${
+        poster
+          ? `<img src="${global.posterUrl}${poster}" alt="${title}" class="card-img-top">`
+          : `<img src="../images/no-image.jpg" class="card-img-top" alt="Movie Title" />`
+      }
+    </div>
+    <div>
+      <h2>${title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>${rating.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${release}</p>
+      <p>${overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${genres.map(genre => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  `;
+  divBottom.innerHTML = `
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${numberWithCommas(budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${numberWithCommas(revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${numberWithCommas(runtime)} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${companies
+      .map(company => `<span>${company.name}</span>`)
+      .join(', ')}</div>
+  `;
+
+  global.movieDetails.appendChild(divTop);
+  global.movieDetails.appendChild(divBottom);
+};
+
+// display backdrop on details pages
+const displayBackgroundImage = (type, backdrop) => {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdrop})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+};
+
 // fetch the data from TMDB API
 const fetchApiData = async endpoint => {
   global.spinner.classList.add('show');
@@ -81,6 +172,11 @@ const highlightActiveLink = () => {
   });
 };
 
+// add commas to number, using regex
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // initialize the app
 const init = () => {
   switch (global.currentPage) {
@@ -95,6 +191,7 @@ const init = () => {
       break;
     case '/movie-details.html':
       console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
